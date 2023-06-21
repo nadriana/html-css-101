92516cb68fea67c40c0b5cb566f28d62380e486d
Adriana <35787696+nadriana@users.noreply.github.com>
2023-06-20 20:46:54 -0600

    Initial commit

---
+++ scripts/nav-script.js
@@ -0,0 +1,46 @@
+// const nav = `
+//     <nav>
+//       <a href="index-no-html.html">Text-only</a>
+//       <a href="index-html.html">HTML-only</a>
+//       <a href="index-html-css.html">HTML &amp; CSS</a>
+//     </nav>
+// `;
+
+const links = [
+  { 'label' : '🏠',
+    'href' : '/index.html'
+  },
+  { 'label' : 'Text',
+    'href' : '/versions/text-only.html'
+  },
+  { 'label' : 'HTML',
+    'href' : '/versions/html-only.html'
+  },
+  { 'label' : 'HTML & CSS',
+  'href' : '/versions/html-css.html'
+  },
+  { 'label' : '🌟 HTML & CSS 🌟',
+    'href' : '/versions/html-css-plus.html'
+  }
+]
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  let body = document.querySelector('body');
+
+  let nav = document.createElement('nav');
+  let ul = document.createElement('ul');
+  
+  for (const link of links) {
+    let anchor = document.createElement('a');
+    anchor.setAttribute('href', link.href);
+    anchor.innerText = link.label;
+    let li = document.createElement('li');
+    li.append(anchor);
+    ul.append(li);
+  }
+  
+  nav.append(ul);
+  nav.setAttribute('class', 'helper-nav');
+  
+  body.prepend(nav);
+});
